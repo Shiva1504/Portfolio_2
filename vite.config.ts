715d68da18899d9a62cfadc7336ef4a76ae5e23f
SHIVA NAIDU <117988290+Shiva1504@users.noreply.github.com>
2025-01-30 17:46:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  base: "/Employee_Assistance_System/",
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
-    base: process.env.NODE_ENV === 'production' 
-  ? '/Portfolio_2/' 
-  : '/',
-  build: {
-    outDir: "dist", // Explicit output directory
-    emptyOutDir: true, // Clear directory before build
-    sourcemap: mode !== 'production', // Disable sourcemaps in production
-  }
-}));
+});
+
