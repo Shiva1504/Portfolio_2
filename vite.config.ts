886f8090e65acdd1c4dbca3e56f40a65ba23a37d
SHIVA NAIDU <117988290+Shiva1504@users.noreply.github.com>
2025-01-30 17:47:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Employee_Assistance_System/",
+  base: "/Portfolio_2/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
